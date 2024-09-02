35d08bc592796857fc7f4b738dbf8bd3db0e250b
onurbosstan <onurbosstan@gmail.com>
2024-09-02 21:10:37 +0300

    Initial Commit

---
+++ WorldSia/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WorldSia
+//
+//  Created by Onur Bostan on 2.09.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
