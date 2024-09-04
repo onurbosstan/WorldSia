9e1d1380739422187bdb8cb9165fc06afff53e2f
onurbosstan <onurbosstan@gmail.com>
2024-09-04 22:07:21 +0300

    API defined by creating WorldRoutes

---
+++ WorldSia/APIs/WorldRoutes.swift
@@ -0,0 +1,12 @@
+//
+//  WorldRoutes.swift
+//  WorldSia
+//
+//  Created by Onur Bostan on 4.09.2024.
+//
+
+import Foundation
+
+class WorldRoutes {
+    static let baseUrl = "https://restcountries.com/v3.1/all"
+}
